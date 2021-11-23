aaaa802d2d674e6579cc9288e839bae9a21068fd
GabrielDuke <42377719+Gabriel018@users.noreply.github.com>
2021-11-23 17:33:26 -0300

    Delete Projeto_Django/catalago directory

--- Projeto_Django/catalago/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Author,Genero,Book,BookInstance
-# Register your models here.
-admin.site.register(Book)
-admin.site.register(Author)
-admin.site.register(Genero)
-admin.site.register(BookInstance)
